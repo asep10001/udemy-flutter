d6b37405c45df7d5f42c2d28921ff298d1ae396b
asep10001 <a2h2hemingway@gmail.com>
2020-11-14 20:38:56 +0700

    init code

---
+++ android/app/src/main/kotlin/com/example/a2h2/first_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a2h2.first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  // This widget is the root of your application.
+  Widget build(BuildContext context){
+    return MaterialApp(home: Text('Hello'),);
+  }
+}
